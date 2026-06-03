ede516c9d5f2f5a86d689da82008036ed351a8ca
Fionn7 <2764786411@qq.com>
2026-06-03 13:17:24 +0000

    VERSION 1.1.1 fix delete errorings

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const GITHUB_BRANCH = process.env.GITHUB_BRANCH || 'main';
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
