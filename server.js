4cce15b4b1408bf2a99dafbff0681e013ea946b0
Fionn7 <2764786411@qq.com>
2026-06-03 12:56:15 +0000

    VERSION 1.0

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3100;
 
 const upload = multer({
   storage: multer.memoryStorage(),
